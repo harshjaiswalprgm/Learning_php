23a36f23b89a9d77f044f75db769ef988fbc7819
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-09-29 20:36:07 +0530

    update

--- Practice/youtube.php
+++ Practice/youtube.php
@@ -1,3 +1,29 @@
-write take two input and find th the sum of it in php 
+<!-- write take two input and find th the sum of it in php 
+
+ -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+
+<body>
 
 
+   <label for="text">Name</label>
+   <input type="text" id="name" name="name">
+
+   <label for="submit"><button>submit</button></label>
+   <p>Hello, " "</p>
+
+
+   <?php
+
+
+   ?>
+</body>
+
+</html>
